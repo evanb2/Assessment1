a208eaf75aed73d6d1f557c41154679fbdba0e88
assessment <evanbutler@MacBook-Pro.local>
2015-03-06 10:41:39 -0800

    create_contact.twig finish

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<?php
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Addressbook.php";
+
+  session_start();
+  if (empty($_SESSION['list_of_contacts'])) {
+    $_SESSION['list_of_contacts'] = array();
+  }
+
+  $app = new Silex\Application();
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
+
+  $app->get("/", function() use ($app) {
+    return $app['twig']->render('contact_homepage.twig', array('contacts' => Contact::getAll()));
+  });
+
+  $app->post("/create_contact", function() use ($app) {
+    $contact = new Contact($_POST['name'], $_POST['number'], $_POST['address']);
+    $contact->save();
+    return $app['twig']->render('create_contact.twig', array('newcontact' => $car));
+  });
+
+  $app->post("/delete_contacts", function() use ($app) {
+    Contact::deleteAll();
+    return $app['twig']->render('delete_contacts.twig');
+  });
+
+  return $app;
+?>
---
+++ src/Addressbook.php
@@ -0,0 +1,56 @@
+<?php
+  class Contact
+  {
+    private $name;
+    private $number;
+    private $address;
+
+    function __construct($name, $number, $address) {
+      $this->name = $name;
+      $this->number = $number;
+      $this->address = $address;
+    }
+
+    //getters
+    function getName() {
+      return $this->name;
+    }
+
+    function getNumber() {
+      return $this->number;
+    }
+
+    function getAddress() {
+      return $this->address;
+    }
+
+    //setters
+    function setName($new_name) {
+      $this->name = (string) $new_name;
+    }
+
+    function setNumber($new_number) {
+      $this->number = (string) $new_number;
+    }
+
+    function setAddress($new_address) {
+      $this->address = (string) $new_address;
+    }
+
+    //functions
+    function save()
+    {
+      array_push($_SESSION['list_of_contacts'], $this);
+    }
+
+    static function getAll()
+    {
+      return $_SESSION['list_of_cars'];
+    }
+
+    static function deleteAll()
+    {
+      $_SESSION['list_of_contacts'] = array();
+    }
+  }
+?>
